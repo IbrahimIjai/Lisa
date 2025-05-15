c9c5ea4763d474fd1d3713ddcba51d206697ca2c
ibrahimijai <sunday.m1701072@st.futminna.edu.ng>
2025-05-15 15:26:48 +0100

    yeah

--- apps/lisa2/app/api/agent/create-agent.ts
+++ apps/lisa2/app/api/agent/create-agent.ts
@@ -1,7 +1,6 @@
 import { getLangChainTools } from "@coinbase/agentkit-langchain";
 import { MemorySaver } from "@langchain/langgraph";
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
-import { ChatOpenAI } from "@langchain/openai";
 import { prepareAgentkitAndWalletProvider } from "./prepare-agentkit";
 import { ChatAnthropic } from "@langchain/anthropic";
 /**
