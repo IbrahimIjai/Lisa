f5d1516cda73fa1d60fe95b65298e22983f6244e
ibrahimijai <sunday.m1701072@st.futminna.edu.ng>
2025-05-16 15:02:14 +0100

    switched to  viem wallet provider

--- apps/lisa/app/api/agent/create-agent.ts
+++ apps/lisa/app/api/agent/create-agent.ts
@@ -1,6 +1,7 @@
 import { getLangChainTools } from "@coinbase/agentkit-langchain";
 import { MemorySaver } from "@langchain/langgraph";
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { ChatOpenAI } from "@langchain/openai";
 import { prepareAgentkitAndWalletProvider } from "./prepare-agentkit";
 import { ChatAnthropic } from "@langchain/anthropic";
 /**
@@ -33,34 +34,39 @@ let agent: ReturnType<typeof createReactAgent>;
  *
  * @throws {Error} If the agent initialization fails.
  */
-export async function createAgent(): Promise<ReturnType<typeof createReactAgent>> {
-  // If agent has already been initialized, return it
-  if (agent) {
-    return agent;
-  }
+export async function createAgent(): Promise<
+	ReturnType<typeof createReactAgent>
+> {
+	// If agent has already been initialized, return it
+	if (agent) {
+		return agent;
+	}
 
-  try {
-    const { agentkit, walletProvider } = await prepareAgentkitAndWalletProvider();
+	try {
+		const { agentkit, walletProvider } =
+			await prepareAgentkitAndWalletProvider();
 
-    // Initialize LLM: https://platform.openai.com/docs/models#gpt-4o
-    const llm = new ChatAnthropic({
+		// Initialize LLM: https://platform.openai.com/docs/models#gpt-4o
+		// const llm = new ChatOpenAI({ model: "gpt-4o-mini" });
+
+		const llm = new ChatAnthropic({
 			model: "claude-3-7-sonnet-20250219",
 			apiKey: process.env.ANTHROPIC_API_KEY,
 		});
 
-    console.log({ keys: process.env.ANTHROPIC_API_KEY });
-    const tools = await getLangChainTools(agentkit);
-    const memory = new MemorySaver();
+		const tools = await getLangChainTools(agentkit);
+		const memory = new MemorySaver();
 
-    // Initialize Agent
-    const canUseFaucet = walletProvider.getNetwork().networkId == "base";
-    const faucetMessage = `If you ever need funds, you can request them from the faucet.`;
-    const cantUseFaucetMessage = `If you need funds, you can provide your wallet details and request funds from the user.`;
-    agent = createReactAgent({
-      llm,
-      tools,
-      checkpointSaver: memory,
-      messageModifier: `
+		// Initialize Agent
+		const canUseFaucet =
+			walletProvider.getNetwork().networkId == "base-sepolia";
+		const faucetMessage = `If you ever need funds, you can request them from the faucet.`;
+		const cantUseFaucetMessage = `If you need funds, you can provide your wallet details and request funds from the user.`;
+		agent = createReactAgent({
+			llm,
+			tools,
+			checkpointSaver: memory,
+			messageModifier: `
         You are a helpful agent that can interact onchain using the Coinbase Developer Platform AgentKit. You are 
         empowered to interact onchain using your tools. ${canUseFaucet ? faucetMessage : cantUseFaucetMessage}.
         Before executing your first action, get the wallet details to see what network 
@@ -70,11 +76,11 @@ export async function createAgent(): Promise<ReturnType<typeof createReactAgent>
         docs.cdp.coinbase.com for more information. Be concise and helpful with your responses. Refrain from 
         restating your tools' descriptions unless it is explicitly requested.
         `,
-    });
+		});
 
-    return agent;
-  } catch (error) {
-    console.error("Error initializing agent:", error);
-    throw new Error("Failed to initialize agent");
-  }
+		return agent;
+	} catch (error) {
+		console.error("Error initializing agent:", error);
+		throw new Error("Failed to initialize agent");
+	}
 }
---
+++ apps/lisa/lib/viem.ts
@@ -0,0 +1,16 @@
+// Initialize WalletProvider:https://docs.cdp.coinbase.com/agentkit/docs/wallet-management
+//https://docs.cdp.coinbase.com/agentkit/docs/wallet-management
+import { ViemWalletProvider } from "@coinbase/agentkit";
+import { privateKeyToAccount } from "viem/accounts";
+import { base } from "viem/chains";
+import { Address, createWalletClient, http } from "viem";
+
+const account = privateKeyToAccount(process.env.PRIVATE_KEY! as Address);
+const client = createWalletClient({
+	account,
+	chain: base,
+	transport: http(),
+});
+const walletProvider = new ViemWalletProvider(client);
+
+export { walletProvider };
